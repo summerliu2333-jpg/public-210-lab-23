14c2e97885466cb436ec3489ae853015c6b9208a
Yang Liu <summerliu2333@gmail.com>
2026-04-02 15:53:30 -0700

    Task one done

--- Goat.h
+++ Goat.h
@@ -13,6 +13,9 @@ private:
 public: 
     Goat()                          { name = ""; age = 0; color = ""; }
     // write three more constructors
+    Goat(string n) : name(n), age(0), color("") {}
+    Goat(string n, int a) : name(n), age(a), color("") {}
+    Goat(string n, int a, string c) : name(n), age(a), color(c) {}
 
     // setters and getters
     void set_name(string n)         { name = n; };
